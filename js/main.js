b5bd7fee9cae99d6ab0f9c6746fe89dc27679c84
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-12-18 12:35:51 +0600

    js complate

--- js/main.js
+++ js/main.js
@@ -2,17 +2,16 @@ const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215);
     const randomCode = "#"+ randomNumber.toString(16);
     document.getElementById("rendomColor").style.backgroundColor = randomCode;
-    document.getElementById("ColorCode").innerText = randomCode;
-}
-const copyCode = () => {
-    navigator.clipboard.writeText(randomCode)
+    colorCode = document.getElementById("ColorCode").innerText = randomCode;
 }
 document.getElementById("button__generate").addEventListener(
     "click", 
     getColor
 )
-document.getElementById("button__copy").addEventListener(
-    "click", 
-    copyCode
-)
 getColor();
+
+document.getElementById("button__copy").addEventListener("click", function () {
+    navigator.clipboard.writeText(colorCode) 
+      .then(alert("Copied to clipboard: " + colorCode))
+      .catch(console.error("Failed to copy"))
+  });
